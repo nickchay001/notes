2d15fc6c9edd023a73574949c525dfa900b30be7
Nick <119807798+nickchay001@users.noreply.github.com>
2023-05-08 15:10:17 +0300

    create-notes-app

---
+++ src/components/NoteInfo/NoteInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NoteInfo.module.scss"
+
+const NoteInfo = () => {
+    return(
+        <div className={styles.noteInfo}>
+            NoteInfo
+        </div>
+    )
+}
+
+export default NoteInfo
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SideBar.module.scss"
+
+const SideBar = () => {
+    return (
+        <aside className={styles.SideBar}>
+            SideBar
+        </aside>
+    )
+}
+
+export default SideBar
